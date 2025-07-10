03ddd10690887717a848328695d95999d2ca0ad4
Wesley Leite <leitewesleyleite@gmail.com>
2025-07-10 09:44:21 -0300

    Updated

--- FastTech.LojaCardapio.Infra/Migrations/20250710124121_update.Designer.cs
+++ FastTech.LojaCardapio.Infra/Migrations/20250710124121_update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FastTech.LojaCardapio.Infra.Persistense;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FastTech.LojaCardapio.Infra.Migrations
 {
     [DbContext(typeof(LojaCardapioDbContext))]
-    partial class LojaCardapioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250710124121_update")]
+    partial class update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
